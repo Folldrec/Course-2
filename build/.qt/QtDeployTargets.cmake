461163a132b23fd757168107c0d23103d7fe728c
Folldrec <max.gaevyi@gmail.com>
2025-11-17 23:24:36 +0200

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Lab_2_FILE D:/Programing/Course 2/build/Lab_2.exe)
-set(__QT_DEPLOY_TARGET_Lab_2_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Lab_2_RUNTIME_DLLS D:/Programing/Qt/6.10.0/mingw_64/bin/Qt6Widgets.dll;D:/Programing/Qt/6.10.0/mingw_64/bin/Qt6Gui.dll;D:/Programing/Qt/6.10.0/mingw_64/bin/Qt6Core.dll)
